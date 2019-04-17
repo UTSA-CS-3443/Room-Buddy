77c7e0035002653f0b08733c3f63b9724c1b48cf
xqh083 <dylan.mall.dm@gmail.com>
2019-04-17 15:31:27 -0500

    sign up controller

---
+++ RoomBuddy/src/application/model/SignUpController.java
@@ -0,0 +1,37 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController implements EventHandler<ActionEvent>, Initializable {
+
+	@FXML TextField fullName;
+	@FXML TextField userName;
+	@FXML PasswordField passPhrase;
+	@FXML PasswordField passPhraseConfirm;
+	@FXML TextField phoneNum;
+	@FXML TextField eMail;
+	@FXML Label passNoMatch;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		if(!passPhrase.getText().equals(passPhraseConfirm.getText()))
+			passNoMatch.setText("Passwords do not match. Please try again.");
+		
+		
+	}
+
+}
