13458e97331a25f9c5a1aae1ca1d50bbad7f456e
Luis Damian Valdes <38168334+ludava@users.noreply.github.com>
2019-04-17 14:59:32 -0500

    Add files via upload

---
+++ RoomBuddy/src/application/controller/LoginController.java
@@ -0,0 +1,122 @@
+package application.controller;
+
+
+import java.util.ArrayList;
+
+import application.Main;
+import application.model.user;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+/**
+ * @author Luis Valdes (qbw322)
+ * UTSA CS 3443 - Lab 5
+ * Spring 2019
+ * Login controller handles the login credentials
+ */
+public class LoginController implements EventHandler<ActionEvent> {
+	public static final String foo = null;
+	public static Object enteredUser;
+	@FXML
+	private TextField userText;
+	@FXML
+	private TextField passText;  
+	@Override
+	/**
+	 * get the user's username and password, then pass that into the validate method inside User.java 
+	 * and check if it exists. Next read in the crew members that belong to same ship register as the user
+	 * and pass them in to the ship through the second controller's texts and then load the Personnel.fxml
+	 */
+	public void handle( ActionEvent event) {
+
+		try {
+			String username = userText.getText();
+			String password = passText.getText();
+			user enteredUser = new user(username,password);
+			
+			//String welcomedUser = enteredUser.welcomeUser(username);
+			
+		
+			
+				String returnedUser = user.validate(username,password);
+				if(returnedUser.equals("false")) {
+				}
+				
+				if(returnedUser.equals("true")) {
+					System.out.println("YUHH");
+				}
+					
+			//	FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Personnel.fxml"));
+			//	Parent root = (Parent) loader.load();
+				
+			//	PersonnelController secController = loader.getController();
+			//	secController.stringName(welcomedUser);
+			//	secController.shipNregister(shipNreg);	
+			//for(int i=0;i<=7;i++) {
+					//if(nameSplit[i].toLowerCase().contains(username)) {
+					//	str =nameSplit[i];
+					//  index = i;
+				//		secController.name1(nameSplit[index]);
+					//}
+				//}
+				/*	if ( nameSplit[3].toLowerCase().indexOf(username.toLowerCase()) != -1 ) {
+						secController.name3(nameSplit[0]);
+					}
+					if(!nameSplit[1].toLowerCase().contains(username)) {
+						secController.name2(nameSplit[1]);
+					}
+					if(!nameSplit[2].toLowerCase().contains(username)) {
+						secController.name3(nameSplit[2]);
+					}
+					if(!nameSplit[3].toLowerCase().contains(username)) {
+						secController.name4(nameSplit[3]);
+					}
+					if(!nameSplit[4].toLowerCase().contains(username)) {
+						secController.name5(nameSplit[4]);
+					}
+					if(!nameSplit[5].toLowerCase().contains(username)) {
+						secController.name6(nameSplit[5]);
+					}
+					if(!nameSplit[6].toLowerCase().contains(username)) {
+						secController.name7(nameSplit[6]);
+					}
+					if(!nameSplit[7].toLowerCase().contains(username)) {
+						secController.name8(nameSplit[7]);
+					}
+					if(nameSplit[1].toLowerCase().contains(username)) {
+						secController.name2(nameSplit[0]);
+					}
+					if(nameSplit[2].toLowerCase().contains(username)) {
+						secController.name3(nameSplit[0]);
+					}
+					if(nameSplit[3].toLowerCase().contains(username)) {
+						secController.name4(nameSplit[0]);
+					}
+					if(nameSplit[4].toLowerCase().contains(username)) {
+						secController.name5(nameSplit[0]);
+					}
+					if(nameSplit[5].toLowerCase().contains(username)) {
+						secController.name6(nameSplit[0]);
+					}
+					if(nameSplit[6].toLowerCase().contains(username)) {
+						secController.name7(nameSplit[0]);
+					}
+					if(nameSplit[7].toLowerCase().contains(username)) {
+						secController.name8(nameSplit[7]);
+					}
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.show();
+				}
+				}
+		*/}catch(Exception e) {
+			//	e.printStackTrace();
+		}		
+	}
+}
---
+++ RoomBuddy/src/application/model/user.java
@@ -0,0 +1,138 @@
+package application.model;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * @author qbw322
+ *UTSA CS 3443 - Lab 5
+ * Spring 2019
+ *this class requires 2 arguments to validate if the user's credentials match
+ *the username, and the password
+ */
+public class user {
+	private String userName;
+	private String passWord;
+	public user(String user, String pass) throws IOException{
+		this.userName = user;
+		this.passWord = pass;
+	//	validate(userName,passWord);
+	//	welcomeUser(userName);
+	}
+	/**
+	 * 
+	 * @param u
+	 * @param p
+	 * @return
+	 * @throws IOException
+	 * the validate method checks if what the user entered is in the system and returns true if they are, 
+	 * and false if they are not
+	 */
+	public static String validate(String u, String p)throws IOException {
+		String validation = "";
+		try {
+			Scanner scan = new Scanner(new File("data/loginUPDATED.csv"));
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				String[] userSplit = line.split(",");
+				if(userSplit[0].equals(u) && userSplit[1].equals(p)){
+					validation = "true";
+				
+					break;
+				}else if((userSplit[0]!=u) && (userSplit[1]!=p)){
+					validation = "false";
+				}
+			}
+		scan.close();
+		}catch( IOException e ) {
+			e.printStackTrace();
+		}
+		return validation;
+	}
+	/**
+	 * @return
+	 * @throws IOException
+	 * this method scans the personnel.csv file and returns which register that the ship in which the 
+	 * user belongs to
+	 */
+	public String returnedRegister()throws IOException {
+		String reg = "";
+		try {
+			Scanner scan = new Scanner(new File("data/personnel.csv"));
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				String[] userSplit = line.split(",");
+				if(userSplit[0].toLowerCase().contains(userName)){
+					reg = userSplit[3] ;
+					break;
+				}
+			}
+		scan.close();
+		}catch( IOException e ) {
+			reg = "";
+			return reg;
+		//	e.printStackTrace();
+			
+		}
+		return reg;
+	}
+	/**
+	 * @param register
+	 * @return
+	 * @throws IOException
+	 * this method returns the name of the fleet the user belongs to
+	 */
+	public String returnedFleetName(String register)throws IOException {
+		String fleetName = "";
+		try {
+			Scanner scan = new Scanner(new File("data/fleet.csv"));
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				String[] userSplit = line.split(",");
+				if(userSplit[1].equals(register)){
+					fleetName = userSplit[0] ;
+					break;
+				}
+			}
+		scan.close();
+		}catch( IOException e ) {
+			fleetName = " ";
+			return fleetName;
+		//	e.printStackTrace();
+			
+		}
+		return fleetName;
+	}
+	public String getName(){
+		return this.userName;
+	}
+	/**
+	 * @param text
+	 * @return
+	 * @throws IOException
+	 * this method returns the Welcome message that shows up when the suer signs in  
+	 */
+	public  String welcomeUser(String text)throws IOException {
+		String str = "";
+		String cap = text.substring(0, 1).toUpperCase() + text.substring(1);
+		try {
+			Scanner scan = new Scanner(new File("data/personnel.csv"));
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				String[] userSplit = line.split(",");
+				if(userSplit[0].toLowerCase().contains(text)){
+					 str = userSplit[2] + " " + cap ;
+					break;
+				}
+				if(!userSplit[0].toLowerCase().contains(text)){
+					str = "No USER";
+				}
+			}
+		scan.close();
+		}catch( IOException e ) {
+			e.printStackTrace();
+		}
+		
+		return str;
+	}
+}
