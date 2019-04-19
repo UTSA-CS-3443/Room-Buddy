37cc63e6703bdc19bcf16cea3640996014ab0e45
xqh083 <dylan.mall.dm@gmail.com>
2019-04-19 14:22:26 -0500

    .

--- RoomBuddy/src/application/model/SignUpController.java
+++ RoomBuddy/src/application/model/SignUpController.java
@@ -3,10 +3,14 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -25,7 +29,15 @@ public class SignUpController implements EventHandler<ActionEvent>, Initializabl
 	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
-
+	
+	@Override
+	public void goBack(ActionEvent arg0) {
+        // Move to previous view - Login.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
+		Main.stage.setScene(new Scene(root, 600, 600));
+		Main.stage.show();
+	}
+	
 	@Override
 	public void handle(ActionEvent arg0) {
 		if(!passPhrase.getText().equals(passPhraseConfirm.getText())) {
