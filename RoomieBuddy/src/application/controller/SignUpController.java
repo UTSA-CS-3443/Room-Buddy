d08bf514bb29f0b2816fff048799b79a92ebee02
xqh083 <dylan.mall.dm@gmail.com>
2019-04-21 15:26:05 -0500

    .

--- RoomieBuddy/src/application/controller/SignUpController.java
+++ RoomieBuddy/src/application/controller/SignUpController.java
@@ -7,23 +7,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import application.Main;
-import application.controller.LoginController;
 
-import java.util.ResourceBundle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.text.Text;
 
 public class SignUpController implements Initializable, EventHandler<ActionEvent>{
 	@FXML
@@ -43,18 +35,11 @@ public class SignUpController implements Initializable, EventHandler<ActionEvent
     
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
 	@Override
 	public void handle(ActionEvent arg0) {
 		try {
-//			System.out.println(passField.getText());
-//			System.out.println(userField.getText());
-//			System.out.println(confirmPassField.getText());
-//			System.out.println(nameField.getText());
-//			System.out.println(emailField.getText());
-//			System.out.println(phoneField.getText());
 			if(passField.getText().equals(confirmPassField.getText())) {
 				FileWriter writer = new FileWriter("data/loginUpdated.csv",true);
 				String str = "\n" + nameField.getText() + "," + userField.getText() + "," + passField.getText() + "," + emailField.getText() + "," + phoneField.getText() ; 
